33d655083d4c541f1cffd79a02b48da8ee1ed87a
crazyfrank <axu9417@gmail.com>
2025-07-28 19:43:53 +0800

    feat(langchainx):add milvus wrap

--- internal/vecstore/vecstore.go
+++ internal/vecstore/vecstore.go
@@ -3,9 +3,11 @@ package vecstore
 import (
 	"context"
 
+	"github.com/milvus-io/milvus-sdk-go/v2/entity"
 	"github.com/tmc/langchaingo/schema"
 	"github.com/tmc/langchaingo/vectorstores"
-	"github.com/tmc/langchaingo/vectorstores/milvus"
+
+	"github.com/crazyfrankie/voidx/pkg/langchainx/milvus"
 )
 
 type VecStoreService struct {
@@ -26,4 +28,12 @@ func (s *VecStoreService) AddDocument(ctx context.Context, documents []schema.Do
 
 func (s *VecStoreService) GetRetriever(numDocs int, opts ...vectorstores.Option) vectorstores.Retriever {
 	return vectorstores.ToRetriever(s.vecStore, numDocs, opts...)
+}
+
+func (s *VecStoreService) DeleteDocumentsByID(ctx context.Context, key string, value any) error {
+	return s.vecStore.DeleteDocument(ctx, key, value)
+}
+
+func (s *VecStoreService) UpdateDocument(ctx context.Context, column ...entity.Column) error {
+	return s.vecStore.UpdateDocument(ctx, column...)
 }
